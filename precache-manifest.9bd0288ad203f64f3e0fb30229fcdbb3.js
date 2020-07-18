09ecce1527d8d91e12528e2b6cf6c4ff49646448
Madhav kabra <mkabra@techracers.io>
2020-07-18 14:34:43 +0530

    Updates

---
+++ precache-manifest.9bd0288ad203f64f3e0fb30229fcdbb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b5a7c301281454a72d82e210904491b",
+    "url": "/TicTacToe/index.html"
+  },
+  {
+    "revision": "54f564ae1eaa6ed690be",
+    "url": "/TicTacToe/static/css/main.a5e81676.chunk.css"
+  },
+  {
+    "revision": "95c0fc93a9fdce6929f2",
+    "url": "/TicTacToe/static/js/2.2bc534d8.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/TicTacToe/static/js/2.2bc534d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54f564ae1eaa6ed690be",
+    "url": "/TicTacToe/static/js/main.38f00339.chunk.js"
+  },
+  {
+    "revision": "ebdbc3f3c36f0e8a7009",
+    "url": "/TicTacToe/static/js/runtime-main.3e2f971d.js"
+  }
+]);
